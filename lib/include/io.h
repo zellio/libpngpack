37629306e3dc339521087644db1ff92238688287
Zachary Elliott <zach@nyu.edu>
2013-07-24 21:20:47 -0400

    renaming type header file

--- lib/include/io.h
+++ lib/include/io.h
@@ -4,7 +4,7 @@
 #define __LIBPNGPACK_TYPE_H__ 1
 
 
-#include "type.h"
+#include "types.h"
 
 
 #ifdef __cplusplus
@@ -17,10 +17,11 @@ extern "C" {
 
 char* memblk_to_s(memblk_t* block);
 int memblk_fwrite(memblk_t* block, FILE* fp);
+//int memblk_fread(memblk_t* block, size_t size, FILE* fp);
 
 char* png_block_to_s(png_block_t* block);
 int png_block_fwrite(png_block_t* block, FILE* fp);
-
+//int png_block_fread(png_block_t* block, FILE* fp);
 
 
 #ifdef __cplusplus
