5ab9db57a3861ac1b4d65cacdfa1dfc1a4792d51
Zachary Elliott <zach@nyu.edu>
2013-07-29 23:28:43 -0400

    Adding png io header file

---
+++ include/io/png.h
@@ -0,0 +1,30 @@
+
+
+#ifndef __LIBPNGPACK_IO_PNG_H__
+#define __LIBPNGPACK_IO_PNG_H__ 1
+
+
+#include "type/byte.h"
+#include "type/png.h"
+
+#include <stdio.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#if 0
+}
+#endif
+
+
+char* png_to_s(png_t* block);
+int png_io_write(png_t* block, FILE* fp);
+int png_io_read(png_t* block, FILE* fp);
+
+
+#ifdef __cplusplus
+};
+#endif
+
+#endif
