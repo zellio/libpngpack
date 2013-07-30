0d0d51b2dfa6b11097192e47ca1394dd1c148fb0
Zachary Elliott <zach@nyu.edu>
2013-07-29 23:20:52 -0400

    Changing io into generic include header

--- include/io.h
+++ include/io.h
@@ -1,31 +1,4 @@
 
 
-#ifndef __LIBPNGPACK_TYPE_H__
-#define __LIBPNGPACK_TYPE_H__ 1
-
-
-#include "types.h"
-
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-#if 0
-}
-#endif
-
-
-char* memblk_to_s(memblk_t* block);
-int memblk_fwrite(memblk_t* block, FILE* fp);
-//int memblk_fread(memblk_t* block, size_t size, FILE* fp);
-
-char* png_block_to_s(png_block_t* block);
-int png_block_fwrite(png_block_t* block, FILE* fp);
-//int png_block_fread(png_block_t* block, FILE* fp);
-
-
-#ifdef __cplusplus
-};
-#endif
-
-#endif
+#include "io/memblk.h"
+#include "io/png_block.h"
